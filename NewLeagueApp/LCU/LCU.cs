2f47da4388948b56c3f8a185ff303c908dbac493
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2019-12-15 23:47:50 -0500

    started working on the LCU apis

---
+++ NewLeagueApp/LCU/LCU.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LCUSharp;
+namespace NewLeagueApp.LCU {
+    class LCU {
+        public LCU()
+        {
+            init();
+        }
+
+        public async void init()
+        {
+            var League = await LeagueClient.Connect();
+            var iconID = new { profileIconId =  0 };
+            var icon = await League.MakeApiRequest(HttpMethod.Put, "/lol-summoner/v1/current-summoner/icon", iconID);
+            Console.WriteLine(icon);
+        }
+    }
+}
